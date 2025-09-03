1ce1e96dc869b35f1f01943e7abaae869a554658
Mailen Ortiz <mailenortiz.m@gmail.com>
2025-09-03 16:35:26 -0300

    Resolución del ejercicio 2

--- Trabajo-Team.py
+++ Trabajo-Team.py
@@ -12,6 +12,20 @@
 # 2. Mailén Ortiz
 # ---------------------------------
 
+# Si se asigna un valor a una variable fuera del rango permitido y el programa no tiene ninguna verificación, el código va a seguir ejecutándose normalmente pero va a generarse un error lógico, aunque no aparezca un mensaje específico de error cuando lo ejecutemos en la terminal. Para evitarlo, se debe validar el valor usando condicionales que, en el caso que sea un valor fuera de rango, muestre un mensaje de error y vuelva a pedir el dato hasta que esté dentro del rango correcto.
+
+#Ejemplo: Si en un bucle se ingresa un valor fuera del rango estipulado este va a generar un mensaje de error y va a pedirle al usuario volver a ingresar un número dentro del rango.
+
+nota = -3 #Se le da un valor fuera de rango para que entre al bucle While 
+
+while (nota < 1) or (nota > 10):
+    nota = int(input("Ingrese una nota en el rango del 1 al 10:")) #Se ingresa una nueva nota
+
+    if (nota >= 1) and (nota <= 10): #Verifica si está en el rango, si no lo está vuelve a ejecutarse el bucle.
+        print("Nota ingresada correctamente")
+    else:
+        print(f"La nota ingresada: {nota}, no es válida. Ingrese una nota en el rango del 1 al 10")
+
 # ---------------------------------
 
 
