905ea4116b60600bb1548e160e928c8fa505d16f
Mailen Ortiz <mailenortiz.m@gmail.com>
2025-09-07 20:53:17 -0300

    Resolución de ejercicio 2 y 8 - Bidimensionales

--- Unidad 5 - Listas/02. TP - Listas Bidimensionales/ejercicio8.py
+++ Unidad 5 - Listas/02. TP - Listas Bidimensionales/ejercicio8.py
@@ -1,3 +1,12 @@
 # Crea un programa que genere una matriz identidad de tamaño n. Una matriz identidad 
 # es una matriz cuadrada donde los elementos de la diagonal principal son 1 y el resto 
 # son 0. - Mailen Ortiz
+
+import numpy as np
+
+tamañoMatriz = int(input("Ingrese el tamaño de la matriz identidad:"))
+
+matrizIdentidad =  [[1 if i == j else 0 for j in range(tamañoMatriz)] for i in range(tamañoMatriz)]
+
+for fila in matrizIdentidad:
+    print(fila)
