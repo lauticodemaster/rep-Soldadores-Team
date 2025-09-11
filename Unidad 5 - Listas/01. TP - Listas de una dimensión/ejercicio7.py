b59871a60e03bd1b37bce52c97815dd8dd0e03f7
Lauticodemaster <lautigamermendoza@gmail.com>
2025-09-11 01:44:21 -0300

    Resolución ejercicio7.py

--- Unidad 5 - Listas/01. TP - Listas de una dimensión/ejercicio7.py
+++ Unidad 5 - Listas/01. TP - Listas de una dimensión/ejercicio7.py
@@ -1,2 +1,25 @@
 # Escribe un programa que permita al usuario ingresar una lista de números y calcule el 
 # promedio de los elementos - Lautaro Fernández
+
+
+# Pedimos al usuario que ingrese los números separados por espacio
+entrada = input("Ingresa una lista de números separados por espacio: ")
+
+# Separamos los números como texto
+numeros_texto = entrada.split()
+
+# Convertimos cada número de texto a entero y los guardamos en una lista
+numeros = []
+for n in numeros_texto:
+    numeros.append(int(n))
+
+# Sumamos todos los números usando un bucle
+suma_total = 0
+for num in numeros:
+    suma_total += num
+
+# Calculamos el promedio (suma total dividido la cantidad de números)
+promedio = suma_total / len(numeros)
+
+# Mostramos el resultado al usuario
+print("El promedio de los números es:", promedio)
